d466dc4a8503c8fc651a03d4a3ef48c861fe7535
lxt0412-ai <lxt0412@gmail.com>
2026-06-21 21:51:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base:'/Temu-/',
   server: {
     host: "0.0.0.0",
-    port: 5173,
-    base:'/Temu-/'
+    port: 5173, 
   },
 });
